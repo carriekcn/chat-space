74a03f85916dbadb23e7c99e265d5a9a07abed63
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-05-29 21:11:21 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,25 @@
 class GroupsController < ApplicationController
+  def new
+    //例
+    //@product = Product.find(params[:product_id])
+    //@review = Review.new
+  end
+
+  def create
+    //例
+    //Review.create(create_params)
+    //redirect_to controller: :products, action: :index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def params_group
+    params.require("group").permit(:name)
+  end
+
 end
