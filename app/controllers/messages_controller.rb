eb0e2d5c09ff4a71ab3009573aba52759a2f8136
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-06-02 15:37:32 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,15 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+    #Message.create(create_params)
+    #redirect_to controller: :groups. actopm: :index
+  end
+
+  private
+  def create_params
+    #params.require(:message).permit(:content, :image).merge()...
+  end
+
 end
