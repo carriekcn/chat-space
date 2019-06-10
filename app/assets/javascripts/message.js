deae46362ed7ca297364a6418b584ffdc36fcf31
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-06-10 23:22:55 +0900

    update messages_controller.rb

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,9 @@
 $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
+    //message = $('.input-box__text').val();
+    console.log(this)
     var formData = new FormData(this);
+    //var url = (window.location.href);
   })
 })
