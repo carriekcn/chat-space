c69d27d5d52f2e9f47d1cbcfdfb136b93a3d3b95
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-06-11 20:54:26 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,6 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    //message = $('.input-box__text').val();
-    console.log(this)
-    var formData = new FormData(this);
-    //var url = (window.location.href);
+    var message = new FormData(this);
   })
-})
+});
