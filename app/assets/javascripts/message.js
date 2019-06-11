3257592997a3558cfb2de90f2682ced49fa23a3b
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-06-11 21:52:41 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,44 @@
 $(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-info">
+                    <p class="upper-info__user">
+                      ${message.user_name}
+                    </p>
+                    <p class="upper-info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message__text">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+  return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
+    var url = (window.location.href);
+    $.ajax({  
       url: url,
-      type: message,
       type: 'POST',
       data: message,
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
     })
   })
 });
