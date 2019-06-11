a978fe8bb294f06ad18f6f19ddf1abb7380f576f
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-06-11 21:02:25 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,5 +2,15 @@ $(document).on('turbolinks:load', function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: message,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 });
