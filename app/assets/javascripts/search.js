b921797a74670778ae0837ca3a5d97948b2144b1
西かおる <carrie@nishikaoru-no-MacBook-Air.local>
2019-06-17 21:21:14 +0900

    update search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,26 @@
 $(function(){
-  $('').on('click', function(e){
-    e.preventDefault();
-    console.log(this);
-  })
-  //console.log("test");
+  $("#user-search-field").on("keyup", function(){
+    var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { name: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      $("#user_search_result").empty();
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else{
+        appendErrMsgToHTML("一致するユーザはいません");
+      }
+    })
+    .fail(function(){
+      alert('error');
+    });
+  });
 });
